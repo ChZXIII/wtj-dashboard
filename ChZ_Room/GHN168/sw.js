dc4711a272b26a64ac835b60e4dae1e324489104
ChZXIII <cheezphoto.contact@gmail.com>
2026-06-19 16:24:19 +0700

    style(ghn168): crop sig_keng whitespace and increase preview signature height

--- ChZ_Room/GHN168/sw.js
+++ ChZ_Room/GHN168/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ghn168-app-v19';
+const CACHE_NAME = 'ghn168-app-v20';
 const ASSETS = [
   'index.html',
   'style.css',
   'app.js',
-  'manifest.json?v=19',
+  'manifest.json?v=20',
   'assets/ghn_app_icon.png',
   'assets/logo.png',
   'assets/sidebar_logo_dark.png',
