8a75c9086c4c928bc3020e6c941d6c3232ea9dd0
ChZXIII <cheezphoto.contact@gmail.com>
2026-06-25 02:09:48 +0700

    feat(ghn168): implement PWA force clear cache button v74

--- ChZ_Room/GHN168/sw.js
+++ ChZ_Room/GHN168/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ghn168-app-v73';
+const CACHE_NAME = 'ghn168-app-v74';
 const ASSETS = [
   'index.html',
-  'style.css?v=73',
-  'app.js?v=73',
-  'manifest.json?v=73',
+  'style.css?v=74',
+  'app.js?v=74',
+  'manifest.json?v=74',
   'assets/ghn_app_icon.png',
   'assets/logo.png',
   'assets/sidebar_logo_dark.png',
